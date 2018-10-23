b327e9bd72d4c155613d6a6301e7b4596a7748bf
Chevy Ray Johnston <happytrash@gmail.com>
2018-10-22 22:07:50 -0700

    package build script

---
+++ GridMap/Assets/Editor/Build.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class Build
+{
+    [MenuItem("Build/Grid Map")]
+    public static void BuildPackage()
+    {
+        var guids = AssetDatabase.FindAssets("", new string[]{
+            "Assets/GridMap",
+            "Assets/Gizmos"
+        });
+
+        var assets = new string[guids.Length];
+        for (int i = 0; i < guids.Length; ++i)
+            assets[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+
+        var file = EditorUtility.SaveFilePanel("Export Package", "Assets/../..", "GridMap", "unitypackage");
+        if (!string.IsNullOrEmpty(file))
+            AssetDatabase.ExportPackage(assets, file, ExportPackageOptions.Default);
+    }
+}
